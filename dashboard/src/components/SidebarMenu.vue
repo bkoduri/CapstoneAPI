db7d6f4326d65658ca75b49168f82979de467ce1
Bhargav Krishna Koduri <kbhargav.95@gmail.com>
2019-04-12 01:02:16 -0400

    Update SidebarMenu.vue

--- dashboard/src/components/SidebarMenu.vue
+++ dashboard/src/components/SidebarMenu.vue
@@ -29,7 +29,7 @@
         </span>
       </a>
       <ul class="treeview-menu">
-        <li class="treeview-title">Employee Tools</li>
+        <li class="page">Employee Tools</li>
         <router-link tag="li" class="pageLink" to="/tasks">
           <a>
             <i class="fa fa-tasks"></i>
@@ -55,7 +55,7 @@
           </a>
         </router-link>
 
-        <li class="treeview-title">PAGES</li>
+        <li class="page">PAGES</li>
         <router-link tag="li" class="pageLink" to="/login">
           <a>
             <i class="fa fa-circle-o text-yellow"></i>
